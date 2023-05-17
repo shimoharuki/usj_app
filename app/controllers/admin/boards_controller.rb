b3857e3aff67a30d6d872a8b33b57977eac4008b
shimoharuki <haruharuharu220@icloud.com>
2023-05-17 15:58:49 +0900

    modify

--- app/controllers/admin/boards_controller.rb
+++ app/controllers/admin/boards_controller.rb
@@ -8,7 +8,7 @@ class Admin::BoardsController < Admin::BaseController
         if @board.save
             redirect_to boards_path, success:"投稿しました"
         else
-            flash.now['danger'] = "失敗しました"
+            flash.now['danger'] = "失敗しましたん"
             render :new
         end
     end
