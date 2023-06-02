94d6d05f6db03f7a36ce3decc25d145a11c6e685
shimoharuki <haruharuharu220@icloud.com>
2023-06-02 14:04:56 +0900

    modfiy

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,7 +4,7 @@ class BoardsController < ApplicationController
         if params[:tag_name].present?
             @boards = @boards.tagged_with(params[:tag_name])
         end
-        @boards = @boards.page(params[:page]).per(40)
+        @boards = @boards.page(params[:page]).per(24)
     end
     def show
         @board = Board.find(params[:id])
