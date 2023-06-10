f21bd8a056cf65b829f19a40e5901a5014aa9b19
shimoharuki <haruharuharu220@icloud.com>
2023-06-10 14:43:12 +0900

    Gemfile.lock fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require admin-lte/plugins/bootstrap/js/bootstrap.bundle.min
 import "@hotwired/turbo-rails"
 import "controllers"
