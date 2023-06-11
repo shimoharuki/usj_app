19e95a8ea05816a0f81927ae57b78ecfcece30d4
shimoharuki <haruharuharu220@icloud.com>
2023-06-11 23:01:34 +0900

    Precompiled assets

--- db/migrate/20230516051151_create_boards.rb
+++ db/migrate/20230516051151_create_boards.rb
@@ -2,7 +2,7 @@ class CreateBoards < ActiveRecord::Migration[7.0]
   def change
     create_table :boards do |t|
       t.references :user, foreign_key: true
-      #t.references :recommendation, foreign_key: true
+      t.references :recommendation, foreign_key: true
       t.string :title
       t.text :body
 
