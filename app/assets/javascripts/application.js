694a9c1509444b5e5c9d0cb43962e85e9ada6360
shimoharuki <haruharuharu220@icloud.com>
2023-06-11 13:10:14 +0900

    modifymigratefaile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require admin-lte/plugins/bootstrap/js/bootstrap.bundle.min
+//= require admin-lte/dist/js/adminlte.mins
 //= require rails-ujs
 //= require jquery
 //= require jquery_ujs
 //= require popper
 //= require bootstrap
 //= require activestorage
-//= require admin-lte/plugins/bootstrap/js/bootstrap.bundle.min
-//= require admin-lte/dist/js/adminlte.mins
