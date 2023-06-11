ecbad5d52e6df70d32af200b3e4f1e57dd9a1e50
shimoharuki <haruharuharu220@icloud.com>
2023-06-11 11:40:11 +0900

    modifymigratefaile

--- db/migrate/20230605032541_create_answers.rb
+++ db/migrate/20230605032541_create_answers.rb
@@ -3,7 +3,7 @@ class CreateAnswers < ActiveRecord::Migration[7.0]
     create_table :answers do |t|
       t.references :question, foreign_key: true
       t.references :user, foreign_key: true
-      t.references :choice, foreign_key: true
+      #t.references :choice, foreign_key: true
 
       t.timestamps
     end
