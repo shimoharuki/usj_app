55860f3070a2c7ef4821715a763b141e354017bd
shimoharuki <haruharuharu220@icloud.com>
2023-06-16 16:40:06 +0900

    Precompiled assets

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -18,7 +18,6 @@ class RecommendationsController < ApplicationController
       @answers.each do |answer|
         @recommendation.answers << answer
       end
-      @recommendation.boards = @recommendation.boards.uniq { |board| board.id }
       @recommendation.save
       redirect_to recommendations_path
     end
